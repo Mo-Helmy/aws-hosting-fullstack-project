15fb132643112932b4117a630899da48024835ea
Mo Helmy <helmysmile3@gmail.com>
2022-11-03 05:28:33 +0000

    edit package.json

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -25,7 +25,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from './controllers/v0/model.index';
   console.log('Database Connected');
 
   const app = express();
-  const port = 3000;
+  const port = process.env.SERVER_PORT;
 
   app.use(bodyParser.json());
 
