d5b09c1837cec46dbf8c5868e9a131c28542114f
Mo Helmy <helmysmile3@gmail.com>
2022-11-03 06:09:43 +0000

    edit package.json

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0',
+  apiHost:
+    'http://hostingfullstackapp-env.eba-3mvfv7aw.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
