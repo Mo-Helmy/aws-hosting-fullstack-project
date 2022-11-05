2e2d8b11e545fa0e1592d0443e13eafa68d85d73
Mo Helmy <helmysmile3@gmail.com>
2022-11-05 23:44:10 +0200

    final steps

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://hosting-fullstack-app-1/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://hosting-fullstack-app-1/
+aws s3 cp --recursive --acl public-read ./www s3://hostingfullstackapp/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://hostingfullstackapp/
